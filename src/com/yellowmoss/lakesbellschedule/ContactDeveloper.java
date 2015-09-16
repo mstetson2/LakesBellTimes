727e28425546d53463b58bd1c29ab1dc1a6c77d4
mstetson2 <mstetson717@gmail.com>
2015-09-16 02:22:23 -0500

    Change to Version 1.0_1

--- src/com/yellowmoss/lakesbellschedule/ContactDeveloper.java
+++ src/com/yellowmoss/lakesbellschedule/ContactDeveloper.java
@@ -1,11 +1,15 @@
 package com.yellowmoss.lakesbellschedule;
 
-import android.net.Uri;
+//import android.net.Uri;
+//import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.app.Activity;
 //import android.view.Menu;
 import android.content.Intent;
+//import java.util.List;
+//import android.content.pm.ResolveInfo;
+import android.text.*;
 
 public class ContactDeveloper extends Activity {
 
@@ -21,7 +25,7 @@ public class ContactDeveloper extends Activity {
 		getMenuInflater().inflate(R.menu.contact_developer, menu);
 		return true;
 	}
-*/
+//
 	public void startSendEmail(View view)
 	{
 		Intent sendEmail = new Intent(Intent.ACTION_SENDTO); // it's not ACTION_SEND
@@ -30,6 +34,22 @@ public class ContactDeveloper extends Activity {
 		sendEmail.putExtra(Intent.EXTRA_TEXT, "Body of email");
 		sendEmail.setData(Uri.parse("mailto:msetetson717@gmail.com")); // or just "mailto:" for blank
 		sendEmail.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // this will make such that when user returns to your app, your app is displayed, instead of the email app.
+		*/
+		
+		public void startSendEmail(View view)
+		{
+		Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+		emailIntent.setType("text/html");
+		emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "testing email send.");
+		emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, Html.fromHtml("<b>this is html text in email body.</b>"));
+		startActivity(Intent.createChooser(emailIntent, "Email to Friend"));
+		}	
+		/**PackageManager packageManager = getPackageManager();
+		List<ResolveInfo> activities = packageManager.queryIntentActivities(sendEmail, 0);
+		boolean isIntentSafe = activities.size() > 0;
+		
+		if(isIntentSafe){
 		startActivity(sendEmail);
-	}
+		*/
+	
 }
